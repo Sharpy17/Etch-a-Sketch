376e772f23db19910a2cd91653fa86d42c32fee6
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-03-03 13:57:10 +0600

    Update the grid

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const container = document.querySelector('.container')
-const square = document.querySelectorAll('div')
-const squareArr = Array.prototype.slice(square);
+const button = document.querySelector('.btn');
+let globalInput = 16;
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div')
-    container.appendChild(square);
-}
+function createGrid() {
+    for (let row = 0; row < globalInput; row++) {   for (let column = 0; column < globalInput; column++) {
+        const square = document.createElement ('div');
+        container.appendChild(square);
+        }
+    }
+};
 
- for (let i = 0; i < squareArr.length; i++) {
-    (function(index) {
-        squareArr[index].addEventListener('click', changeColor(i)); 
+createGrid();
+
+button.addEventListener('click', () => {
+    let userInput = prompt('Number of squares', 16);
+    window.globalInput = userInput
 });
 
-function changeColor(i) {
- return function() {
-     console.log("you clicked region number " + i);
- };
+function updateGrid() {
+    
+}
+
+const square = document.querySelectorAll('div');
+const squareArr = Array.prototype.slice(square);
+
+for (let i = 0; i < squareArr.length; i++) {
+    squareArr[i].addEventListener("click", changeColor);
+
+    function changeColor(i) {
+        $(i).css('backgroundColor', 'blue');
+    }   
 };
+
+
+
+
+
+
