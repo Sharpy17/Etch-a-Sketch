5610438d66c8ecd346c8cd4d15d20aeb96336d11
askarbekov-d-auca-2022 <askarbekov_d@auca.kg>
2023-03-05 13:45:14 +0600

    Complete a preliminary version

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-const container = document.querySelector('.container')
+const body = document.querySelector('.huy');
 const button = document.querySelector('.btn');
-let globalInput = 16;
-
-function createGrid() {
-    for (let row = 0; row < globalInput; row++) {   for (let column = 0; column < globalInput; column++) {
-        const square = document.createElement ('div');
-        container.appendChild(square);
-        }
-    }
-};
-
-createGrid();
+let color = document.getElementById('colorselect').value;
 
 button.addEventListener('click', () => {
     let userInput = prompt('Number of squares', 16);
-    window.globalInput = userInput
+    if (userInput <= 100) {
+        window.globalInput = userInput;
+    } else {
+        window.globalInput = 16;
+    }
+    updateGrid();
 });
 
-function updateGrid() {
-    
-}
-
-const square = document.querySelectorAll('div');
-const squareArr = Array.prototype.slice(square);
-
-for (let i = 0; i < squareArr.length; i++) {
-    squareArr[i].addEventListener("click", changeColor);
-
-    function changeColor(i) {
-        $(i).css('backgroundColor', 'blue');
-    }   
+function createGrid(input) {
+    const container = document.createElement('p');
+    container.classList.add('container');
+    container.style['grid-template-columns'] = `repeat(${input}, 1fr)`;
+    container.style['grid-template-rows'] = `repeat(${input}, 1fr)`;
+    body.appendChild(container);
+    window.cont = container;
+        for (let row = 0; row < input; row++) {   for (let column = 0; column < input; column++) {
+            const square = document.createElement ('div');
+            container.appendChild(square);
+            const squar = document.querySelectorAll('div')
+            window.squareList = squar;
+            }
+                squareList.forEach((item) => {
+                    item.addEventListener('mouseover', () =>
+                    item.style.backgroundColor = `${color}`);
+                });
+            
+        }
 };
 
+createGrid(16);
 
+function updateGrid() {
+    cont.remove();
+    createGrid(globalInput);
+}
 
+//const square = document.querySelectorAll('div');
+//const squareArr = Array.from(square);
+
+//function changeColor(i) {
+//    i.style.backgroundColor = 'black';
+//}   
+
+//for (let i = 0; i < squareArr.length; i++) {
+//    squareArr[i].addEventListener('mouseover', changeColor);
+//}
+
+console.log(color);
 
 
 
